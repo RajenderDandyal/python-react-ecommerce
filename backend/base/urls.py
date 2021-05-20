5c65c64f7838e026d91286b94d50912c5db75ac4
rajender.dindyal <rajender.dindyal@energiapp.com>
2021-05-20 17:30:10 +0530

    yup

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -17,6 +17,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('user/login/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('user/profile/', views.getUserProfile, name="user profile"),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getProduct, name="products"),
